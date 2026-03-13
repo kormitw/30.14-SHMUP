197cdb8961e9189cddecd10ac053e5a2853c8dbe
kormitw <roger.candari@student.ateneo.edu>
2026-03-13 20:49:18 +0800

    updated shooting mechanics

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 @export var health = 10
-@export var speed = 300
+@export var speed = 500
 const PROJECTILE = preload("res://scenes/projectile.tscn")
 @onready var progress_bar: ProgressBar = $CanvasLayer/ProgressBar
+@onready var shoot_timer: Timer = $shootTimer
 
 func _ready():
 	progress_bar.max_value = health
@@ -17,11 +18,10 @@ func _process(delta: float) -> void:
 		velocity = Vector2.ZERO
 	move_and_slide()
 
-	if Input.is_action_just_pressed("shoot"):
-		var new_projectile = PROJECTILE.instantiate()
-		new_projectile.global_position = global_position
-		add_sibling(new_projectile)
-
+func _on_shoot_timer_timeout() -> void:
+	var new_projectile = PROJECTILE.instantiate()
+	new_projectile.global_position = global_position
+	add_sibling(new_projectile)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemy"):
